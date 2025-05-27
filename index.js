91bf62d9aa62f5a39d1441f43652ad89a916dee7
ishpreet501 <sishpreet135@gmail.com>
2025-05-27 11:20:11 +0530

    finally

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ import enquiryRouter from "./Routes/enquiry.js";
 import adminRouter from "./Routes/Admin.js";
 const port = 5000;
 const server = express();
-server.use(cors())
+server.use(cors({
+  origin:["http://localhost:5173/","https://sparkling-snickerdoodle-1cff85.netlify.app/"
+  ],
+  credentials:true
+}))
 server.use(express.json())
 
 server.use("/api",enquiryRouter)
