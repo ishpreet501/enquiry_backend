0cfad8dabc5f0347643225572f377f5a45743e6c
ishpreet501 <sishpreet135@gmail.com>
2025-05-27 12:15:24 +0530

    finally

--- Controller/Admin.js
+++ Controller/Admin.js
@@ -35,19 +35,26 @@ export const registerAdmin = async (req, res) => {
     }
   };
   
-export const loginAdmin = async (req,res) => {
-    const {adminEmail,password} = req.body;
-    try {
-        const Admin = await AdminModel.findOne({ adminEmail})
+export const loginAdmin = async (req, res) => {
+    const { adminEmail, password } = req.body;
 
-      const isMatch = await bcrypt.compare(password, Admin.password);
-          if (!isMatch) {
+    try {
+        const Admin = await AdminModel.findOne({ adminEmail });
+
+        if (!Admin) {
             return res.status(401).json({ success: false, message: "Invalid credentials" });
-          }
-          const token = jwt.sign({ id: Admin._id }, "secret", { expiresIn: "1h" });
-          
-              res.json({ success: true, token });
+        }
+
+        const isMatch = await bcrypt.compare(password, Admin.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ success: false, message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: Admin._id }, "secret", { expiresIn: "1h" });
+
+        res.json({ success: true, token });
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
-}
+};
